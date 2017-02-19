52045e21888f5e190c32d5991d4936f343bb599d
Dzianis Zhaunerchyk <evrenior@mail.ru>
2017-02-18 22:37:05 -0500

    cleanup

--- index.php
+++ index.php
@@ -7,19 +7,19 @@
 </head>
 <body>
     <pre>
-    1. <a href="login.php">login.php</a> Data storage for connection to db server - <b>denis</b> 
-    2. <a href="form.php">form.php</a> Getting data from array <b>POST</b> as an array to output
-    3. <a href="index.php">index.php</a> This file
-    4. <a href="add_pro.php">add_pro.php</a> Adding book using method <b>prepare</b> to db
-    5. <a href="form.html">form.html</a> Check of different <b>INPUT</b> in HTML forms
-    6. <a href="mysqlrunner.php">mysqlrunner.php</a> Output from <b>denis_table</b>  db all the books
-    7. <a href="login_auth.php">login_auth.php</a>File of loging in with PHP with redirect to session
-    8. <a href="continue.php">continue.php</a> Continuation file of <b>login_auth.php</b> after check login/password with saving to session
-    9. <a href="mysql.php">mysql.php</a> Output all the books form <b>denis_table</b> db with green background and button <b>LINK</b> 
-    10. <a href="books.php">books.php</a> Test task from<b>ROGUE FITNESS</b>  with listing all the books with sort by first letter in the word
-    11. <a href="phpdev.php">phpdev.php</a> Check for <b>class inheretance</b> and check of usage of<b>mktime(), date(), time(), extract(), explode()</b> functions
-    12. <a href="new_mysql.php">new_mysql.php</a> Script for adding, removing and book show in table <b>denis_table</b> 
-    13. <a href="auth.php">auth.php</a> Creation of users table <b>users</b> and adding new users with check of data entered
+        1. <a href="login.php">login.php</a> Data storage for connection to db server - <b>denis</b> 
+        2. <a href="form.php">form.php</a> Getting data from array <b>POST</b> as an array to output
+        3. <a href="index.php">index.php</a> This file
+        4. <a href="add_pro.php">add_pro.php</a> Adding book using method <b>prepare</b> to db
+        5. <a href="form.html">form.html</a> Check of different <b>INPUT</b> in HTML forms
+        6. <a href="mysqlrunner.php">mysqlrunner.php</a> Output from <b>denis_table</b>  db all the books
+        7. <a href="login_auth.php">login_auth.php</a>File of loging in with PHP with redirect to session
+        8. <a href="continue.php">continue.php</a> Continuation file of <b>login_auth.php</b> after check login/password with saving to session
+        9. <a href="mysql.php">mysql.php</a> Output all the books form <b>denis_table</b> db with green background and button <b>LINK</b> 
+        10. <a href="books.php">books.php</a> Test task from<b>ROGUE FITNESS</b>  with listing all the books with sort by first letter in the word
+        11. <a href="phpdev.php">phpdev.php</a> Check for <b>class inheretance</b> and check of usage of<b>mktime(), date(), time(), extract(), explode()</b> functions
+        12. <a href="new_mysql.php">new_mysql.php</a> Script for adding, removing and book show in table <b>denis_table</b> 
+        13. <a href="auth.php">auth.php</a> Creation of users table <b>users</b> and adding new users with check of data entered
     </pre>    
 </body>
 </html>
